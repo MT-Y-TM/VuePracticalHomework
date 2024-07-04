355e6bef790f637d8f67c4d40bd5a35083a7076a
Yui <1906506454@qq.com>
2024-07-04 10:14:30 +0800

    美化代码结构，复制出来的勾式代码结构

--- ReiMi/Reimi/src/pages/subpages/Category.vue
+++ ReiMi/Reimi/src/pages/subpages/Category.vue
@@ -1,37 +1,34 @@
 <template>
-    <div>
-        
+  <div>
     <el-button type="primary" style="margin-bottom: 10px" @click="addRow">新增分类</el-button>
-   <!-- 分类管理 -->
- <el-table ref="tableRef" :data="tableData" style="margin-bottom: 20px
- " row-key="id" border default-expand-all>
-  <el-table-column prop="name" label="分类名称" sortable />
-  <el-table-column label="分类级别">
-  <template #default="{ row }">
-  <span v-if="row.pid == 0">一级分类</span>
-  <span v-else>二级分类</span>
-  </template>
-  </el-table-column>
-  <el-table-column prop="id" label="分类编号" />
-  <el-table-column label="分类图片">
-  <template #default="{ row }">
-  <el-image v-if="row.picture != ''" :src="row.picture" fit="contain" style="display: flex; align-items: center; height: 60px" />
-  </template>
-  </el-table-column>
-  <el-table-column fixed="right" label="操作" width="180">
-  <template #default="{ row }">
-  <el-button type="warning" @click="editRow(row)">编辑</el-button>
-  <el-button type="danger" @click="delRow(row)">删除</el-button>
-  </template>
-  </el-table-column>
-  </el-table>
-  </div>
-  <el-button type="primary" style="margin-bottom: 10px" @click="addRow">新增分类</el-button>
+    <!-- 分类管理 -->
+    <el-table ref="tableRef" :data="tableData" style="margin-bottom: 20px" row-key="id" border default-expand-all>
+      <el-table-column prop="name" label="分类名称" sortable />
+      <el-table-column label="分类级别">
+        <template #default="{ row }">
+          <span v-if="row.pid == 0">一级分类</span>
+          <span v-else>二级分类</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="id" label="分类编号" />
+      <el-table-column label="分类图片">
+        <template #default="{ row }">
+          <el-image v-if="row.picture != ''" :src="row.picture" fit="contain" style="display: flex; align-items: center; height: 60px" />
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template #default="{ row }">
+          <el-button type="warning" @click="editRow(row)">编辑</el-button>
+          <el-button type="danger" @click="delRow(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     <!-- 新增分类的弹出框 -->
     <el-dialog v-model="dialogVisible" :title="id ? '修改分类' : '新增分类'" :before-close="handleBeforeClose">
-        <CategoryEdit ref="categoryForm"  :id="id" @success="editSuccess" />
+      <CategoryEdit ref="categoryForm" :id="id" @success="editSuccess" />
     </el-dialog>
- </template>
+  </div>
+</template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -39,83 +36,80 @@ import { getCategoryList } from '../../api'
 import { ElMessageBox } from 'element-plus'
 import CategoryEdit from '../../components/CategoryEdit.vue'
 
-
 const tableData = ref([])
 const dialogVisible = ref(false)
 const id = ref(0)
 const categoryForm = ref()
 
 onMounted(() => {
- loadCategoryList()
+  loadCategoryList()
 })
 
 // 查询分类列表
 const loadCategoryList = async () => {
- let data = await getCategoryList()
- tableData.value = convertToTree(data)
+  let data = await getCategoryList()
+  tableData.value = convertToTree(data)
 }
 
 // 将一维数组转换成树形结构的方法
 const convertToTree = data => {
- const treeData = []
- const map = {}
- // 遍历一维数组数据，建立节点映射表
- for (const item of data) {
- map[item.id] = { ...item, children: [] }
- }
+  const treeData = []
+  const map = {}
+  // 遍历一维数组数据，建立节点映射表
+  for (const item of data) {
+    map[item.id] = { ...item, children: [] }
+  }
 
- // 遍历映射表，将节点添加到父节点的 children 中
- for (const item of data) {
- const node = map[item.id]
- if (item.pid === 0) {
- treeData.push(node)
- } else {
- const parent = map[item.pid]
- parent.children.push(node)
- }
- }
- return treeData
+  // 遍历映射表，将节点添加到父节点的 children 中
+  for (const item of data) {
+    const node = map[item.id]
+    if (item.pid === 0) {
+      treeData.push(node)
+    } else {
+      const parent = map[item.pid]
+      parent.children.push(node)
+    }
+  }
+  return treeData
 }
 
 // 关闭弹出框前调用的函数
 const handleBeforeClose = () => {
- ElMessageBox.confirm('确定关闭对话框吗？', {
- showClose: false,
- closeOnClickModal: false,
- confirmButtonText: '确定',
- cancelButtonText: '取消',
- }).then(() => {
- dialogVisible.value = false
- }).catch(() => {})
+  ElMessageBox.confirm('确定关闭对话框吗？', {
+    showClose: false,
+    closeOnClickModal: false,
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+  }).then(() => {
+    dialogVisible.value = false
+  }).catch(() => {})
 }
 
 const editSuccess = () => {
- loadCategoryList()
- dialogVisible.value = false
+  loadCategoryList()
+  dialogVisible.value = false
 }
-
-
 
 // 新增分类
 const addRow = () => {
-    if (categoryForm.value) {
- categoryForm.value.resetForm(0)
- }
-
-    id.value = 0
- dialogVisible.value = true
+  if (categoryForm.value) {
+    categoryForm.value.resetForm(0)
+  }
+  id.value = 0
+  dialogVisible.value = true
 }
+
 // 修改分类
 const editRow = row => {
-    if (categoryForm.value) {
- categoryForm.value.resetForm(row.id)
- }
-
-    id.value = row.id
- dialogVisible.value = true
+  if (categoryForm.value) {
+    categoryForm.value.resetForm(row.id)
+  }
+  id.value = row.id
+  dialogVisible.value = true
 }
+
 // 删除分类
 const delRow = row => {
+  // 删除逻辑待实现
 }
-
 </script>
