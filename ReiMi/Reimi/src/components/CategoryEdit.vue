d1ec9caaec0b6c3e855fb4a21ce8947fc69c7ed2
Yui <1906506454@qq.com>
2024-07-04 10:23:51 +0800

    实现到8.5.6

--- ReiMi/Reimi/src/components/CategoryEdit.vue
+++ ReiMi/Reimi/src/components/CategoryEdit.vue
@@ -17,6 +17,27 @@
         <el-option v-for="item in categoryList" :key="item.id" :label="item.name" :value="item.id" />
       </el-select>
     </el-form-item>
+    <!-- 分类图片 -->
+<el-form-item label="分类图片" v-show="showMore">
+    <el-upload
+    ref="uploadRef"
+    class="upload-demo"
+    v-model:file-list="fileList"
+    :action="uploadPictureURL()"
+    :headers="{ jwt: token }"
+    :data="{ type: 'category_picture' }"
+    :limit="1"
+    :on-exceed="handleExceed"
+    :on-success="uploadSuccess"
+    >
+    <template #trigger>
+    <el-button type="primary">请选择图片</el-button>
+</template>
+<template #tip>
+<div class="el-upload__tip">图片文件大小不超过 500KB</div>
+</template>
+</el-upload>
+</el-form-item>
     <!-- 操作按钮 -->
     <el-form-item>
       <el-button type="primary" @click="editSubmit" v-if="id">修改</el-button>
@@ -28,10 +49,15 @@
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
-import { getCategory, getCategoryList } from '../api';
+import { getCategory, getCategoryList,uploadPictureURL } from '../api';
+import useToken from '../stores/token'
+
 
 const categoryList = ref([]);
 const showMore = ref(false);
+const fileList = ref([])
+const uploadRef = ref()
+const { token } = useToken()
 
 const props = defineProps({
   id: {
@@ -61,8 +87,37 @@ const editSubmit = () => {
 // 重置表单
 const btnCancel = () => {
   formRef.value.resetFields();
+  form.picture = ''
+ uploadRef.value.clearFiles()
   loadCategory();
 };
+
+// 文件超出个数限制时替换已有图片
+const handleExceed = files => {
+ uploadRef.value.clearFiles()
+ uploadRef.value.handleStart(files[0])
+ uploadRef.value.submit()
+}
+
+// 上传成功
+const uploadSuccess = response => {
+ const { errno, errmsg, data } = response
+ if (errno !== 0) {
+ notification({
+ message: errmsg,
+ type: 'error'
+ })
+ } else {
+ if (errmsg !== '') {
+ notification({
+ message: errmsg,
+ type: 'success'
+ })
+ }
+ form.picture = data.savepath
+ }
+}
+
 
 onMounted(() => {
   loadCategory();
@@ -71,6 +126,13 @@ onMounted(() => {
 const loadCategory = async () => {
   if (form.id) {
     const data = await getCategory({ id: form.id });
+    if (data.picture !== '') {
+ const fileName = data.picture.substring(data.picture.lastIndexOf('/') + 1)
+if (fileName) {
+ fileList.value = [{ name: fileName, url: data.picture }]
+ }
+ }
+
     Object.assign(form, data);
   }
   const list = await getCategoryList();
@@ -84,3 +146,11 @@ const resetForm = id => {
 
 defineExpose({ resetForm });
 </script>
+
+<style scoped>
+.upload-demo {
+ text-align: left;
+ width: 91%;
+}
+</style>
+
